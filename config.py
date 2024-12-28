261672e9a237ca383e8cfa42bf09f173c492f18b
Test126k <test126126k@gmail.com>
2024-12-28 23:53:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,16 +15,16 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7714466772:AAGX4YKqTQQQAro8lJoycxBGpv6QZ5IsEvs")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "26300022"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "def44e13defba9d104323e821955dfa3")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002414583408"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6057768840"))
@@ -33,19 +33,19 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6057768840"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Dost:Ak997110@cluster0.9qw9v.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", " -1001928037346"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "" ) 
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI am a permenant file store bot made for [ Munni badnaam leaks ] and users can access stored messages by using a shareable link given by  Munni badnaam leaks Admin" ) 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6057768840").split()):
+    for x in (os.environ.get("ADMINS", "6057768840 5295407868").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
