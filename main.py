effb8c23f1e13280e9e9bf73514dd35a4ed4f35e
Test126k <test126126k@gmail.com>
2024-12-28 17:44:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from pyrogram import Client
 bot = Client(
     "my_bot",
     api_id=26300022,  # Your API ID
-    api_hash="def44e13defba9d104323e821955dfa3"  # Your API Hash
+    api_hash="def44e13defba9d104323e821955dfa3",  # Your API Hash
+    bot_token="7714466772:AAGX4YKqTQQQAro8lJoycxBGpv6QZ5IsEvs"  # Replace with your bot's token
 )
 
 bot.run()
