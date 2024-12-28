ec84ba666d03e3090e2f3a0a87155595661b5e92
Test126k <test126126k@gmail.com>
2024-12-28 17:33:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from pyrogram import Client
 
 bot = Client(
     "my_bot",
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH")
+    api_id=int(os.environ.get("26300022")),
+    api_hash=os.environ.get("def44e13defba9d104323e821955dfa3")
 )
 
 bot.run()
